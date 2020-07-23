f6346f97759bfc59b006d1e3ded9bfd9dbe58370
Hugo Gomes <hdsg0807@gmail.com>
2020-07-23 00:10:47 -0300

    Change to https

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 const io = require('socket.io')(server);
 
 app.use(cors());

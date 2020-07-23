c6fd7b7bd0aadb77947a3bb9394fa2e381b78b34
Hugo Gomes <hdsg0807@gmail.com>
2020-07-22 23:29:06 -0300

    Back to http

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const server = require('https').createServer(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
